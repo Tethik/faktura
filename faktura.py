37fb66db76daf6df951bd6a844fd056ef6f64f37
Joakim Uddholm <joakim@uddholm.com>
2015-02-15 18:44:27 +0100

    Some functionality to render a pdf from html jinja template.

---
+++ faktura.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, render_template, send_file
+import pdfkit
+
+app = Flask(__name__)
+# app.config.from_pyfile("faktura.cfg")
+app.config.Debug = True
+
+@app.route('/')
+def hello():
+    return "hello"
+
+@app.route('/list')
+def list():
+    pass
+
+@app.route('/create')
+def create():
+    pass
+
+@app.route('/render', methods=['GET', 'POST'])
+def render():
+    pdfkit.from_string(render_template('render.html'), 'out.pdf')
+    return send_file('out.pdf')
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
