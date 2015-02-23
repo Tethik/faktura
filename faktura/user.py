128e15d5fce8f278aa7d34d99379a1c5e83fd0e5
Joakim Uddholm <joakim@uddholm.com>
2015-02-23 08:06:39 +0100

    Change password functionality.

--- faktura/user.py
+++ faktura/user.py
@@ -3,6 +3,7 @@ from flask import render_template, request
 from faktura.models import db, User
 from faktura.breadcrumbs import breadcrumbs
 from flask.ext.login import login_required
+from passlib.hash import sha256_crypt
 
 @app.route("/user/<int:user_id>", methods=["POST", "GET"])
 @login_required
@@ -10,7 +11,19 @@ def user(user_id):
     user = User.query.filter_by(id=user_id).first()
     msg = ""
     if request.method == "POST":
-        user.email = request.form["email"]
-        db.session.commit()
-        msg = "Saved successfully."
+        if request.form["action"] == "details":
+            user.email = request.form["email"]
+            db.session.commit()
+            msg = "Changed email successfully."
+        elif request.form["action"] == "changepassword":
+            user = User.query.filter_by(id=user_id).first()
+            if not sha256_crypt.verify(request.form["old_password"], user.password):
+                msg = "Old password did not match."
+            elif request.form["new_password"] != request.form["rep_password"]:
+                msg = "New and repeated password do not match."
+            else:
+                user.password = sha256_crypt.encrypt(request.form["new_password"])
+                msg = "Password changed."
+                db.session.commit()
+
     return render_template("users/show.html", user=user, msg=msg, breadcrumbs=breadcrumbs("Main Menu", "Settings"))
